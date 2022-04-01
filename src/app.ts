a0945899b7527e7008951cabd6fe8f63d6c8faf5
Marko Kalgre <marko.kalgre@cybexer.com>
2022-04-01 20:22:21 +0300

    Use correct methods for listening to commands

--- src/app.ts
+++ src/app.ts
@@ -8,20 +8,19 @@ const app = new App({
     logLevel: LogLevel.DEBUG
 });
 
-app.message('/mariostart' || ':mario:', async ({message, say}) => {
-    if (isGenericMessageEvent(message))
-        await say(`Mario Time! <@${message.user}>`);
+app.command('/mariostart', async ({command, say}) => {
+        await say(`Mario Time! <@${command.user_name}>`);
 });
 
-app.message('/marioend', async ({say}) => {
+app.command('/marioend', async ({say}) => {
     await say('Hey! Come back here! You big-a monkey! - Ending poll.');
 });
 
-app.message('/mariofinish' || ':checkered_flag:', async ({say}) => {
+app.command('/mariofinish', async ({say}) => {
     await say('Wahoo! I will ask for positions in the leaderboard.');
 });
 
-app.message('/mariostats', async ({say}) => {
+app.command('/mariostats', async ({say}) => {
    await say('Here we go! Stats are:');
 });
 
