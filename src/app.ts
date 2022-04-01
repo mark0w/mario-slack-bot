e48745ca1fd97dac44cabf5279f6d80d7af93155
Marko Kalgre <marko.kalgre@cybexer.com>
2022-04-01 21:20:29 +0300

    Create basic functionality for Mario Kart team fillment

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import {App, LogLevel} from '@slack/bolt'
 
+let acceptedPlayers: string[];
+
 const app = new App({
     token: process.env.BOT_TOKEN,
     appToken: process.env.SLACK_APP_TOKEN,
@@ -7,12 +9,63 @@ const app = new App({
     logLevel: LogLevel.DEBUG
 });
 
-app.command('/mariostart', async ({command, say}) => {
-        await say(`Mario Time! <@${command.user_name}>`);
+app.command('/mariostart', async ({say}) => {
+    await say({
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'plain_text',
+                    text: 'Would You like to join Mario Kart?'
+                }
+            },
+            {
+                type: 'actions',
+                block_id: 'mario_kart_poll',
+                elements: [
+                    {
+                        type: 'button',
+                        text: {
+                            type: 'plain_text',
+                            text: ':white_check_mark:'
+                        },
+                        action_id: 'player_accepted'
+                    },
+                    {
+                        type: 'button',
+                        text: {
+                            type: 'plain_text',
+                            text: ':x:'
+                        },
+                        action_id: 'player_declined'
+                    }
+                ]
+            }
+        ]
+    });
 });
 
+app.action('player_accepted', async ({body, ack, say}) => {
+    await ack();
+    if (acceptedPlayers.length == 4) {
+        await say('Mama mia... Slots are full...');
+        return;
+    }
+    acceptedPlayers.push(body.user.id);
+    await say(`Let-s a go! ${body.user.id} joined: ${4 - acceptedPlayers.length} slots left!`);
+    if (acceptedPlayers.length == 4) await say('Slots are filled! Happy karting!')
+});
+
+app.action('player_declined', async ({body, ack, say}) => {
+    await ack();
+    if (acceptedPlayers.includes(body.user.id)) {
+        delete acceptedPlayers[acceptedPlayers.indexOf(body.user.id)];
+        await say(`${body.user.id} Hey! Come back here! You big-a monkey!`);
+    } else await say('So long, eh Bowser!');
+})
+
 app.command('/marioend', async ({say}) => {
-    await say('Hey! Come back here! You big-a monkey! - Ending poll.');
+    await say('Buh-bye! - Ending poll.');
 });
 
 app.command('/mariofinish', async ({say}) => {
@@ -20,7 +73,7 @@ app.command('/mariofinish', async ({say}) => {
 });
 
 app.command('/mariostats', async ({say}) => {
-   await say('Here we go! Stats are:');
+    await say('Here we go! Stats are:');
 });
 
 (async () => {
